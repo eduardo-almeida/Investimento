993aa9158ab4ea2348fc3767b2d986b842721f65
Eduardo Almeida <eduardoeandroid@gmail.com>
2021-07-22 12:57:54 -0300

    Aula 14

---
+++ carteira/tasks/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.db import models
+from django.forms import fields
+
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+
+    class Meta:
+        model = Task
+        fields = ("nome", "valor", "modalidade")
